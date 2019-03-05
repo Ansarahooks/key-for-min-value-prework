b49d135fc1f8a1163f72287c4d32f51223e331f1
ansara.hooks <ansara.hooks@gmail.com>
2019-03-05 18:20:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
     nil
   else
     name_hash.each do |key, value|
-      [0..value].length
+      my_array = (0..value).to_a
+      my_array.length
     end
   end
   #name_hash.index(value)
